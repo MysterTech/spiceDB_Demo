af59425f3b9efa622bb4f5d8733c0123d1bd6d98
C M Sanjay Krishna <sanjay.krishna@mystertech.com>
2022-04-15 02:37:02 +0530

    [Add] spicedb client app structure and gradle files

---
+++ spicedb-client/src/main/java/ventures/captl/spicedbclient/SpicedbClientApplication.java
@@ -0,0 +1,13 @@
+package ventures.captl.spicedbclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpicedbClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpicedbClientApplication.class, args);
+	}
+
+}
---
+++ spicedb-client/src/test/java/ventures/captl/spicedbclient/SpicedbClientApplicationTests.java
@@ -0,0 +1,13 @@
+package ventures.captl.spicedbclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpicedbClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
